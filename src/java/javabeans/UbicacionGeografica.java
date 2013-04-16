866020f99051e0a1d95ad38b2e9a8271ab0ede69
SENA <SENA@DOSDFPCDI7F425.mshome.net>
2013-04-15 20:20:43 -0500

    modifiado por Jonatan

--- src/java/javabeans/UbicacionGeografica.java
+++ src/java/javabeans/UbicacionGeografica.java
@@ -4,65 +4,64 @@ import org.json.simple.JSONObject;
 
 public class UbicacionGeografica 
 {
-    private String id_usuario;
-    private String lantitud;
-    private String pais;   
+    private String id_ubicacion;
+    private String latitud;
+    private String longitud;   
     
     
     public UbicacionGeografica()
     {
-        this.id_usuario = "";
-        this.lantitud = "";
-        this.pais = "";     
-       
+        this.id_ubicacion = "";
+        this.latitud = "";
+        this.longitud = "";
     }
     
-    public UbicacionGeografica(String id_usuario, String lantitud, String pais, String departamento, String ciudad)
+    public UbicacionGeografica(String id_ubicacion, String latitud, String longitud)
     {
-        this.id_usuario = id_usuario;
-        this.lantitud = lantitud;
-        this.pais = pais;
+        this.id_ubicacion = id_ubicacion;
+        this.latitud = latitud;
+        this.longitud = longitud;
        
     }
     
     public JSONObject getJSONObject() 
     {
         JSONObject obj = new JSONObject();
-        obj.put("id_usuario", this.id_usuario);
-        obj.put("lantitud", this.lantitud);
-        obj.put("pais", this.pais);        
+        obj.put("id_ubicacion", this.id_ubicacion);
+        obj.put("latitud", this.latitud);
+        obj.put("longitud", this.longitud);        
                            
         return obj;
     }
     
-    public void setID_usuario(String id_usuario)
+    public void setID_usuario(String id_ubicacion)
     {
-        this.id_usuario = id_usuario;
+        this.id_ubicacion = id_ubicacion;
     }
     
     public String getID_usuario()
     {
-        return this.id_usuario;
+        return this.id_ubicacion;
     }
     
-    public void setLatitud(String lantitud)
+    public void setLatitud(String latitud)
     {
-        this.lantitud = lantitud;
+        this.latitud = latitud;
     }
     
     public String getLatitud()
     {
-        return this.lantitud;
+        return this.latitud;
     }
     
-    public void setPais(String pais)
+    public void setLongitud(String longitud)
     {
-        this.pais = pais;
+        this.longitud = longitud;
     }
     
-    public String getPais()
+    public String getLongitud()
     {
-        return this.pais;
+        return this.longitud;
     }    
           
 }
