95dae972bf7b8c25fb6137737cb1a18a625681b2
SENA <SENA@DOSDFPCDI7F425.mshome.net>
2013-04-15 21:01:09 -0500

    modifiado por Jonatan

--- src/java/modelo/pedidosSQL.java
+++ src/java/modelo/pedidosSQL.java
@@ -108,4 +108,296 @@ public class pedidosSQL {
      
         return pedido;
     }
+    
+    public JSONArray obtenerListadoPedidos(JSONObject jsonObject)
+    {
+       JSONArray Pedidos = new JSONArray();
+       
+       try
+       {
+           this.cn = getConnection();
+           this.st = cn.createStatement();
+           String fechaInicial= String.valueOf(jsonObject.get("fechaInicio"));
+           String fechaFinal= String.valueOf(jsonObject.get("fechaFin"));
+           String tsql;            
+           if("".equals(fechaInicial))
+           {
+               if("".equals(fechaFinal))
+               {
+                tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                        + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente;";   
+                this.rs = this.st.executeQuery(tsql);                  
+               }
+           }
+           else
+           {
+               tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                       + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                       + " pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"';";
+               this.rs = this.st.executeQuery(tsql); 
+           }
+
+           while(this.rs.next())
+           {
+               JSONObject pedido = new JSONObject();
+               pedido.put("id_pedido",rs.getString("id_pedido"));
+               pedido.put("fecha",rs.getString("fecha"));
+               pedido.put("razon_social",rs.getString("razon_social"));
+               pedido.put("nombre_usuario",rs.getString("nombre_usuario"));
+               pedido.put("direccion",rs.getString("direccion"));
+               pedido.put("valor_total",rs.getString("valor_total"));
+               System.out.printf(pedido.toString());
+               Pedidos.add(pedido); 
+           }
+           this.desconectar();
+       }
+
+       catch(Exception e)
+       {
+           e.printStackTrace();            
+       }
+
+       return(Pedidos);
+   }
+    
+    public JSONArray obtenerListadoPedidosCliente(JSONObject jsonObject)
+    {
+       JSONArray Pedidos = new JSONArray();
+       
+       try
+       {
+           this.cn = getConnection();
+           this.st = cn.createStatement();
+           String fechaInicial= String.valueOf(jsonObject.get("fechaInicio"));
+           String fechaFinal= String.valueOf(jsonObject.get("fechaFin"));
+           String nombre_del_cliente= String.valueOf(jsonObject.get("nombre_del_cliente"));
+           String id_cliente= String.valueOf(jsonObject.get("id_cliente"));
+           String tsql = null;  
+           
+           if("".equals(nombre_del_cliente))
+           {
+               if("".equals(id_cliente))
+               {
+                   if("".equals(fechaInicial))
+                   {
+                       if("".equals(fechaFinal))
+                       {
+                           tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                                   + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente;";
+                           this.rs = this.st.executeQuery(tsql);
+                       }
+                   }
+                   else
+                   {
+                       tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                               + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                               + " pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"';";   
+                       this.rs = this.st.executeQuery(tsql);
+                   }
+               }
+               else if("".equals(fechaInicial))
+               {                   
+                    if("".equals(fechaFinal))
+                    {
+                        tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                                + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                                + " pedidos.id_cliente='"+ id_cliente +"';";   
+                        this.rs = this.st.executeQuery(tsql);
+                    }                   
+               }
+               else
+               {
+                   tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                           + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                           + " pedidos.id_cliente='"+ id_cliente +"' AND pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"';";   
+                   this.rs = this.st.executeQuery(tsql);
+               }
+           }
+           else if("".equals(id_cliente))
+           {
+               if("".equals(fechaInicial))
+               {
+                   if("".equals(fechaFinal))
+                   {
+                        tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion "
+                                + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                                + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_cliente +"%';";
+                        this.rs = this.st.executeQuery(tsql);
+                   }
+               }
+               else
+               {
+                   tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                           + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                           + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_cliente +"%' AND pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"';";   
+                   this.rs = this.st.executeQuery(tsql);  
+               }
+           }
+           else
+           {
+               if("".equals(fechaInicial))
+               {
+                   if("".equals(fechaFinal))
+                   {
+                        tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                                + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                                + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_cliente +"%' AND pedidos.id_cliente='"+ id_cliente +"';";
+                        this.rs = this.st.executeQuery(tsql);                        
+                   }
+               }
+               else
+               {
+                   tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                           + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                           + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_cliente +"%' AND pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"' AND id_cliente='"+ id_cliente +"';";   
+                   this.rs = this.st.executeQuery(tsql); 
+               }
+           }
+           
+           System.out.print(tsql.toString());
+           
+           
+           while(this.rs.next())
+           {
+               JSONObject pedido = new JSONObject();
+               pedido.put("id_pedido",rs.getString("id_pedido"));
+               pedido.put("fecha",rs.getString("fecha"));
+               pedido.put("razon_social",rs.getString("razon_social"));
+               pedido.put("nombre_usuario",rs.getString("nombre_usuario"));
+               pedido.put("direccion",rs.getString("direccion"));
+               pedido.put("valor_total",rs.getString("valor_total"));
+               System.out.printf(pedido.toString());
+               Pedidos.add(pedido);
+           }
+           this.desconectar();
+       }
+
+       catch(Exception e)
+       {
+           e.printStackTrace();            
+       }
+
+       return(Pedidos);
+   }
+    
+    
+    public JSONArray obtenerListadoPedidosProducto(JSONObject jsonObject)
+    {
+       JSONArray Pedidos = new JSONArray();
+       
+       try
+       {
+           this.cn = getConnection();
+           this.st = cn.createStatement();
+           String fechaInicial= String.valueOf(jsonObject.get("fechaInicio"));
+           String fechaFinal= String.valueOf(jsonObject.get("fechaFin"));
+           String nombre_del_producto= String.valueOf(jsonObject.get("nombre"));
+           String codigo_producto= String.valueOf(jsonObject.get("codigo_producto"));
+           String tsql = null;  
+           
+           if("".equals(nombre_del_producto))
+           {
+               if("".equals(codigo_producto))
+               {
+                   if("".equals(fechaInicial))
+                   {
+                       if("".equals(fechaFinal))
+                       {
+                           tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                                   + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente;";
+                           this.rs = this.st.executeQuery(tsql);
+                       }
+                   }
+                   else
+                   {
+                       tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                               + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                               + " pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"';";   
+                       this.rs = this.st.executeQuery(tsql);
+                   }
+               }
+               else if("".equals(fechaInicial))
+               {                   
+                    if("".equals(fechaFinal))
+                    {
+                        tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                                + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                                + " pedidos.id_cliente='"+ codigo_producto +"';";   
+                        this.rs = this.st.executeQuery(tsql);
+                    }                   
+               }
+               else
+               {
+                   tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                           + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                           + " pedidos.id_cliente='"+ codigo_producto +"' AND pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"';";   
+                   this.rs = this.st.executeQuery(tsql);
+               }
+           }
+           else if("".equals(codigo_producto))
+           {
+               if("".equals(fechaInicial))
+               {
+                   if("".equals(fechaFinal))
+                   {
+                        tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion "
+                                + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                                + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_producto +"%';";
+                        this.rs = this.st.executeQuery(tsql);
+                   }
+               }
+               else
+               {
+                   tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                           + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                           + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_producto +"%' AND pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"';";   
+                   this.rs = this.st.executeQuery(tsql);  
+               }
+           }
+           else
+           {
+               if("".equals(fechaInicial))
+               {
+                   if("".equals(fechaFinal))
+                   {
+                        tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                                + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                                + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_producto +"%' AND pedidos.id_cliente='"+ nombre_del_producto +"';";
+                        this.rs = this.st.executeQuery(tsql);                        
+                   }
+               }
+               else
+               {
+                   tsql = "SELECT DISTINCT pedidos.id_pedido, pedidos.fecha, pedidos.valor_total, clientes.razon_social, usuarios.nombre_usuario, clientes.direccion"
+                           + " FROM pedidos, clientes, usuarios WHERE pedidos.id_usuario=usuarios.id_usuario AND pedidos.id_cliente=clientes.id_cliente AND"
+                           + " CONCAT(clientes.nombre,' ',clientes.apellido) LIKE '%"+ nombre_del_producto +"%' AND pedidos.fecha BETWEEN '"+ fechaInicial +"' AND '"+ fechaFinal +"' AND id_cliente='"+ codigo_producto +"';";   
+                   this.rs = this.st.executeQuery(tsql); 
+               }
+           }
+           
+           System.out.print(tsql.toString());
+           
+           
+           while(this.rs.next())
+           {
+               JSONObject pedido = new JSONObject();
+               pedido.put("id_pedido",rs.getString("id_pedido"));
+               pedido.put("fecha",rs.getString("fecha"));
+               pedido.put("razon_social",rs.getString("razon_social"));
+               pedido.put("nombre_usuario",rs.getString("nombre_usuario"));
+               pedido.put("direccion",rs.getString("direccion"));
+               pedido.put("valor_total",rs.getString("valor_total"));
+               System.out.printf(pedido.toString());
+               Pedidos.add(pedido);
+           }
+           this.desconectar();
+       }
+
+       catch(Exception e)
+       {
+           e.printStackTrace();            
+       }
+
+       return(Pedidos);
+   }
 }
