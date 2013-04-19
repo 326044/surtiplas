a68742d4d9f5cd84b3556ab4a62f4222b1739fba
SENA <SENA@DOSDFPCDI7F425.mshome.net>
2013-04-18 21:46:47 -0500

    modifiado por Jonatan

--- web/scripts/interaccionVendedor.js
+++ web/scripts/interaccionVendedor.js
@@ -2624,22 +2624,7 @@ function AdicionarBusquedaClientes(jsonArray)
     activadorEventosClientes();
 }
 
-function SerializeToJson(form)
-{
-    var o = {};
-    var a = form;
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-}
+
 
 //**********************************************************************************
 //**********************************************************************************
@@ -3394,22 +3379,7 @@ function AddCliente()
     activadorEventosClientes();
 }
 
-function SerializeToJson(form)
-{
-    var o = {};
-    var a = form;
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-}
+
 
 function enviarDatosAddCliente(evento)
 {
